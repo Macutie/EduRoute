c445d8688f3cd9dc6eb4584c3555fa37b6c90504
Macutie <macelroybadar@gmail.com>
2026-05-03 22:22:21 +0800

    Fix favicon 404 and register SW on load for PWA

--- eduroute_frontend/main.jsx
+++ eduroute_frontend/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerFirebaseServiceWorker } from './lib/firebase.js'
+
+// Register service worker immediately to satisfy PWA requirements
+registerFirebaseServiceWorker().catch(console.error);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
