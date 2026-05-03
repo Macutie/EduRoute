2c213ac17d825576e51c0ae6a407429867cc87f4
Macutie <macelroybadar@gmail.com>
2026-05-03 08:23:14 +0800

    Remove duplicate src folder - using eduroute_frontend instead

--- src/components/notifications/EnableNotificationsButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EnableNotificationsButton = ({ onClick, loading = false, disabled = false }) => (
-  <button type="button" onClick={onClick} disabled={loading || disabled}>
-    {loading ? 'Enabling...' : 'Enable Notifications'}
-  </button>
-);
-
-export default EnableNotificationsButton;
