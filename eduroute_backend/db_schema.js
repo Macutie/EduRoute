7c5b44ad7d6f37dcf2876e187d08e3af41249795
Macutie <146634963+Macutie@users.noreply.github.com>
2026-05-18 07:42:15 +0800

    Delete eduroute_backend/db_schema.js

--- eduroute_backend/db_schema.js
+++
@@ -1,7 +0,0 @@
-const pool = require('./src/db/pool');
-async function run() {
-  const res = await pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'locator_slips'");
-  console.log(res.rows);
-  process.exit(0);
-}
-run();
