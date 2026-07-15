af8052a6bc76de12c7de6281a0ea99686f916940
Macutie <macelroybadar@gmail.com>
2026-07-15 16:32:46 +0800

    Added SMTP address family

--- eduroute_backend/src/config/mailer.js
+++ eduroute_backend/src/config/mailer.js
@@ -1,10 +1,14 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 const env = require('./env');
+
+dns.setDefaultResultOrder?.('ipv4first');
 
 const transporter = nodemailer.createTransport({
     host: env.smtpHost,
     port: env.smtpPort,
     secure: env.smtpSecure,
+    family: Number(process.env.SMTP_ADDRESS_FAMILY || 4),
     connectionTimeout: Number(process.env.SMTP_CONNECTION_TIMEOUT_MS || 10000),
     greetingTimeout: Number(process.env.SMTP_GREETING_TIMEOUT_MS || 10000),
     socketTimeout: Number(process.env.SMTP_SOCKET_TIMEOUT_MS || 15000),
